9786d7804c5f1508e0b28329cf154a7940de9368
Nandan T S <nanven9@gmail.com>
2025-12-23 20:18:53 +0530

    Added boolean parsing support initial

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,13 +1,22 @@
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.Arrays;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 import static java.lang.System.err;
 import static java.lang.System.out;
 
 public class Main {
+    static Set<String> supportedCommands=new HashSet<>();
+
+
+
     public static void main(String[] args) {
+        supportedCommands.add("tokenize");
+        supportedCommands.add("parse");
 
         if (args.length < 2) {
             System.err.println("Usage: ./your_program.sh tokenize <filename>");
@@ -17,7 +26,7 @@ public class Main {
         String command = args[0];
         String filename = args[1];
 
-        if (!command.equals("tokenize")) {
+        if (!supportedCommands.contains(command)) {
             System.err.println("Unknown command: " + command);
             System.exit(1);
         }
@@ -28,25 +37,31 @@ public class Main {
             List<Token> tokenList = Tokenizer.tokenize(fileContents);
             boolean error=false;
 
-            for (Token token:tokenList){
-                if(token.getType()==TokenType.UNEXPECTED_CHARACTER_ERROR){
-                    error=true;
-                    err.printf("[line %d] Error: Unexpected character: %s\n",token.getLine(),token.getLexeme());
-                }
-                else if(token.getType()==TokenType.UNTERMINATED_STRING_ERROR){
-                    error=true;
-                    err.printf("[line %d] Error: Unterminated string.\n",token.getLine());
-                }
-                else{
-                    out.println(token);
-                }
+            if(command.equals("tokenize")){
+                for (Token token:tokenList){
+                    if(token.getType()==TokenType.UNEXPECTED_CHARACTER_ERROR){
+                        error=true;
+                        err.printf("[line %d] Error: Unexpected character: %s\n",token.getLine(),token.getLexeme());
+                    }
+                    else if(token.getType()==TokenType.UNTERMINATED_STRING_ERROR){
+                        error=true;
+                        err.printf("[line %d] Error: Unterminated string.\n",token.getLine());
+                    }
+                    else{
+                        out.println(token);
+                    }
 
-            }
-            out.println("EOF  null");
+                }
+                out.println("EOF  null");
 
-            if(error){
-                System.exit(65);
+                if(error){
+                    System.exit(65);
+                }
             }
+            else if(command.equals("parse")) {
+                out.println(fileContents);
+            }
+
 
         } catch (Exception e) {
             e.printStackTrace();
