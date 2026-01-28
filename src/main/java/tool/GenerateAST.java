248a7cfaec04df691bc902a520a3547e2fa131e9
Nandan T S <nanven9@gmail.com>
2026-01-28 07:34:35 +0530

    Added visitor pattern for generate ast function

--- src/main/java/tool/GenerateAST.java
+++ src/main/java/tool/GenerateAST.java
@@ -10,7 +10,7 @@ import static java.lang.System.out;
 
 public class GenerateAST {
     public static void main(String[] args) throws IOException {
-        if(args.length!=1){
+        if (args.length != 1) {
             System.err.println("Usage: generate_ast <output directory>");
             System.exit(64);
         }
@@ -21,7 +21,7 @@ public class GenerateAST {
         String outputDir = args[0];
 
 
-        defineAST(outputDir,"Expr", Arrays.asList(
+        defineAST(outputDir, "Expr", Arrays.asList(
                 "Binary   : Expr left, Token operator, Expr right",
                 "Grouping : Expr expression",
                 "Literal  : Object value",
@@ -31,28 +31,42 @@ public class GenerateAST {
     }
 
     private static void defineAST(String outputDir, String className, List<String> types) throws IOException {
-        String path=outputDir+className+".java";
-        PrintWriter writer=new PrintWriter(path, StandardCharsets.UTF_8);
+        String path = outputDir + className + ".java";
+        PrintWriter writer = new PrintWriter(path, StandardCharsets.UTF_8);
 
         writer.println("import java.util.List;");
         writer.println();
-        writer.println("abstract class "+className+"{");
+        writer.println("abstract class " + className + "{");
 
-        for(String type:types){
-            String innerClassName=type.split(":")[0].trim();
-            String fields=type.split(":")[1].trim();
-            String[] fieldsSplitList= fields.split(",");
-            writer.println("    static class "+innerClassName+"{");
+        defineVisitor(writer, className, types);
+        writer.println();
+        writer.println("    abstract <R> R accept(Visitor<R> visitor);");
+        writer.println();
+
+        for (String type : types) {
+            String innerClassName = type.split(":")[0].trim();
+            String fields = type.split(":")[1].trim();
+            String[] fieldsSplitList = fields.split(",");
+            writer.println("    static class " + innerClassName+" extends "+className + "{");
             writer.println();
 
-            for(String field:fieldsSplitList){
-                writer.println("        final "+field.trim()+";");
+            for (String field : fieldsSplitList) {
+                writer.println("        final " + field.trim() + ";");
             }
 
-            writer.println("        public "+innerClassName+"("+fields+")"+"{");
-            for(String field:fieldsSplitList){
-                String fieldVariableName=field.trim().split(" ")[1];
-                writer.println("            this."+fieldVariableName+"="+fieldVariableName+";");
+            writer.println();
+            writer.println("        @Override");
+            writer.println("        <R> R accept(Visitor<R> visitor) {");
+            writer.println("            return visitor.visit" +
+                    innerClassName + className + "(this);");
+            writer.println("        }");
+            writer.println();
+
+            writer.println("        public " + innerClassName + "(" + fields + ")" + "{");
+
+            for (String field : fieldsSplitList) {
+                String fieldVariableName = field.trim().split(" ")[1];
+                writer.println("            this." + fieldVariableName + "=" + fieldVariableName + ";");
             }
             writer.println("        }");
             writer.println("    }");
@@ -60,7 +74,20 @@ public class GenerateAST {
         }
 
         writer.println("}");
-        writer.close();;
+        writer.close();
 
+
+    }
+
+    private static void defineVisitor(PrintWriter writer, String baseName, List<String> types) {
+        writer.println("    interface Visitor<R> {");
+
+        for (String type : types) {
+            String typeName = type.split(":")[0].trim();
+            writer.println("        R visit" + typeName + baseName + "(" +
+                    typeName + " " + baseName.toLowerCase() + ");");
+        }
+
+        writer.println("    }");
     }
 }
