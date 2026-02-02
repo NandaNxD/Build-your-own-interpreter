144c36323fca3898400d4fd4f32307cf6b3edbd1
Nandan T S <nanven9@gmail.com>
2026-02-02 22:30:58 +0530

    Added A ST PRINTER

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -59,7 +59,15 @@ public class Main {
                 }
             }
             else if(command.equals("parse")) {
-                out.println(fileContents);
+                Expr expression = new Expr.Binary(
+                        new Expr.Unary(
+                                new Token(TokenType.MINUS, "-", null, 1),
+                                new Expr.Literal(123)),
+                        new Token(TokenType.STAR, "*", null, 1),
+                        new Expr.Grouping(
+                                new Expr.Literal(45.67)));
+
+                System.out.println(new ASTPrinter().print(expression));
             }
 
 
